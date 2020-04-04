ffb2fefc479fb24d8ece5c9275e67acfe6f40aa7
Calum Smeaton <smeatsbeats@gmail.com>
2020-04-04 15:27:52 -0400

    add content

--- index.php
+++ index.php
@@ -19,6 +19,107 @@
           <audio id="ttwwAudioFile" src="audio/TTWW_TEST_MASTER_10.wav" controls></audio>
       </div>
     </section>
+    <section id="content">
+      <h1>To Those Who Wait</h1>
+      <p>What began as an exercise to ease cognitive disonance,
+         somehow took on a life of its own, becoming an earnest
+         attempt to shed light on <em>Waiting</em> as a Mode of Being.
+      </p>
+      <h2>Easier Said Than Sung</h2>
+      <p>
+        As a sound engineer with serious difficulties keeping my opinions to myself,
+        I needed to establish some authority on the basis of which to justify shoving my opinions
+        into the faces of actual recording artists. <br><br>
+        <!--
+        "Let's run another take, but with a bit more <em>energy</em> this time. <br>
+        Does that make sense? Of course it does. Great. <br>
+        Okay, Rolling." <br><br>
+        For some reason, that next take still lacked the precision and clarity that it needed. <br>
+        And the next take. The clock ticks, the meter runs, frustration surfaces. <br><br>
+        But what can I do? It's not my fault if you can't perform your track the way it sounds in your head. <br>
+        I'm just here to record what you give me. <br>
+        Sure. All sorts of analog and digital <em>processing</em> can be applied to the recording after the fact, but this will
+        introduce unexpected <em>color</em> and a result that just isn't the same as an outright inspired performace. <br><br>
+        Halfway through the session, we are still missing those crucial three decibels around four kilohertz. <br>
+        But how the hell do you tease those three decibels out of an artist without turning their session into an episode of <em>Bill Nye the Science Guy?</em> <br>
+        Maybe this time I'll tell them to do it with more <em>Atitude.</em> <br><br>
+        This could only go on so long. <br>
+        As fulfilling as it was, claiming status and power on the basis of some shiny gear, it was time for a reality sandwich. <br>
+        Telling someone again and again that they could be doing something better-without being able to demonstrate how or why-gets old. <br>
+        -->
+        I decided the only thing for it was to step out of my comfort zone. <br>
+        I needed to climb down from my pulpit at the console and get in front of a microphone. <br>
+        I needed experience.
+      </p>
+      <h2>Two Birds With One Phone</h2>
+      <p>
+        The first thing I experienced in front of the mic was speechlessness. <br>
+        Was I cured of my inability to shut up already? <br>
+        Unfortunately (for anyone still willing to work with me) I didn't give up so easily. <br>
+        But I did have to concede that coming up with something worth recording is a daunting task in itself. <br><br>
+        I turned to a three-year-old field recording that I found on an old hard drive. <br>
+        It was accompanied by a note in which my younger self vowed to claim poetic revenge. <br>
+        The audio file was a recording of one of those traffic lights, tricked out with audio to assist blind people--though I'm certain
+        that this one mostly saved clueless students, drifting towards the curb with their faces glued to some screen. <br>
+        "Wait... Wait... Wait..." <br>
+        To the more alert pedestrians, it was annoying. <br>
+        To a nerd like me, it was poetry. <br><br>
+        My poet had a limited arsenal of literary devices, and an even more limited vocabulary... <br>
+        But I couldn't deny that his careful diction and subtle mastery of repitition fit the meaning of his piece. <br>
+        What struck me most of all, was the masked suggestiveness: I was stuck in the Mode of Being of Waiting. <br>
+        Maybe a lot of us were. <br>
+        Quite the critique from a traffic light! <br><br>
+        I probably really began to appreciate it around midterms. <br>
+        A time when you're forced to question the loyalty of your own two feet. <br>
+        "You can turn around," I remind myself. <br>
+        "You are a grown-ass man and can walk striaght out of here." <br>
+        "Wait" suggests the light. <br>
+        The traitors comply. <br><br>
+        So that's how it's going to be.
+      </p>
+      <h2>Yolo</h2>
+      <p>
+        Three years later the opportunity presents itself. <br>
+        No more waiting. Time for some good old fashioned poetic justice. <br>
+        As he twisted my feet aginst my will I will twist his words in my music. <br><br>
+        Now I had something to record and if I did it right, I might just escape that poem's cruel circling.
+      </p>
+      <h2>Defeat</h2>
+      <p>
+        How or when it happened, I haven't the foggiest idea. <br>
+        I thought I had left it far enough in the past. <br>
+        Apparently that is no way to think about it. <br>
+        Though my feet, still ashamed, managed to resist his commanding tone this time around, my hand faltered. <br>
+      <!--
+        I scraped together the lyrics of the first track from some notes I had scrawled down my second week at school. <br>
+        At the time, they seemed to fit the tone, so I went with it. <br>
+        In hindsight, probably not my best strategic move. <br>
+        I listened to that first track again and again until I thought I knew what had to come next. <br><br>
+      -->
+        The project seemed to write itself. <br>
+        I convinced myself that this was creativity or inspiration or something. <br>
+      <!--
+        If it can be called anything like that, it was more like a Haunting In Connecticut style demon-posession. <br>
+      -->
+        Before my eyes,
+        the calculated, spite-born plot to transcend that poem forever melted instead into a <em>thesis</em> on its central theme. <br>
+        A Phenomenology of Waiting.
+      </p>
+
+      <p>
+        So, my plan for revenge failed bitterly and I tanked my shot at any technical musical authority in the process. <br>
+        Frantic to render that evasive <em>image</em> visible, I amplified creaky piano pedals, included artifact-filled samples, and - worst of all - sang falsetto. <br>
+        The result is something that will never entice potential clients or bolster my mixing portfolio. <br>
+        Unlike my tormentor's masterpiece which is broadcast on street <em>crossings</em> nationwide, <br>
+        very few poeple will ever hear this. <br>
+        Most of the people who do will find it to be an unworthy interuption of their screen time. <br>
+        It won't save you from an oncoming vehicle. <br>
+        Some people will just find it annoying. <br>
+        Maybe it was all for nothing. But maybe not. <br>
+        Maybe after a few listens, you will start to appreciate the diction, the repitition, the not-so-subtle suggestiveness. <br>
+        If you're really unlucky, maybe, the murky image of that cruel, comical Mode of Being will even creep tentatively into focus. <br>
+      </p>
+    </section>
 
   </body>
 </html>
