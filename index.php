ac122aa192eb4fb0266a2e762b7826ec2db1a332
Calum Smeaton <smeatsbeats@gmail.com>
2020-04-08 19:49:32 -0400

    revert back to state before credits

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To Those Who Wait</title>
-    <link rel="stylesheet" href="css/ttww.css?v=33.4" type="text/css">
+    <link rel="stylesheet" href="css/ttww.css?v=33.6" type="text/css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script type="text/javascript" src="js/ttww.js?v=1.9"></script>
     <!--google fonts to include -->
@@ -40,11 +40,11 @@
       </div>
 
 
-      <!-- widget section --> 
+      <!-- widget section -->
       <div class="widget" id="widget_boi">
 
 
-        
+
 
       </div>
 
