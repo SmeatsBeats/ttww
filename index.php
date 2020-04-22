38f43af815ff4c59f203882042e00e891cb20f51
Calum Smeaton <smeatsbeats@gmail.com>
2020-04-21 23:32:05 -0400

    add fill hilight on hover for sticks

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To Those Who Wait</title>
-    <link rel="stylesheet" href="css/ttww.css?v=47.4" type="text/css">
+    <link rel="stylesheet" href="css/ttww.css?v=47.9" type="text/css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
     <script type="text/javascript" src="js/ttww.js?v=13.1"></script>
@@ -51,11 +51,11 @@
             </a>
 
               <svg style="width:5%; height:50%;" class="widget_stick" id="a_stick">
-                <rect width="100%" height="100%" style="fill: #999">
+                <rect width="100%" height="100%">
               </svg>
 
               <svg style="width:5%; height:50%;" class="widget_stick" id="b_stick">
-                <rect width="100%" height="100%" style="fill: #999">
+                <rect width="100%" height="100%">
               </svg>
 
           </div>
